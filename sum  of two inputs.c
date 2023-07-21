ca7029547302a89f2ce658a28f07b1bb258053f4
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-07-21 17:48:35 +0600

    Add files via upload

---
+++ sum  of two inputs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the value of first number=");
+    scanf("%d",&a);
+    printf("enter the value value of second number=");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("sum of two numbers=%d\n",sum);
+}
